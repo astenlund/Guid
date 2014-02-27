7e81f3d9c89d60c6626777831e309332985ee649
Andreas Stenlund (asd) <andreas.stenlund@tobii.com>
2014-02-27 10:53:38 +0100

    Initial add

---
+++ Guid/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Guid
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var guid = System.Guid.NewGuid().ToString();
+            Clipboard.SetText(guid);
+            Console.WriteLine(guid);
+        }
+    }
+}
